56e9a59e655fe2be015e7501b30ef838a049c34f
pilvikala <michal.brutvan@gmail.com>
2022-08-26 17:09:18 +0300

    add failing unit test

--- app/src/test/java/io/bitrise/sample/android/ExampleUnitTest.kt
+++ app/src/test/java/io/bitrise/sample/android/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addition_isIncorrect() {
+        assertEquals(4, 2 + 3)
+    }
 }
