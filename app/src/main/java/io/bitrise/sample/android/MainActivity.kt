e011d9b43460fc500778fcf955885ea2356b65e1
Yi-Te Lu <yite.lu@bitrise.io>
2022-08-31 16:31:07 -0700

    gh test branch test1

--- app/src/main/java/io/bitrise/sample/android/MainActivity.kt
+++ app/src/main/java/io/bitrise/sample/android/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        //github test1
+        //github branch 1
+        //hey1
+     }
 }
